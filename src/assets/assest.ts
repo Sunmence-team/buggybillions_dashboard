52cffa04eb5aa01903b1f46edf002e0061e0ba52
Mayorkun27 <156526127+Mayorkun27@users.noreply.github.com>
2026-01-29 14:42:13 +0100

    Review and adjustments

--- src/assets/assest.ts
+++ src/assets/assest.ts
@@ -1,9 +1,7 @@
 import logo from "./images/logo.png";
-import chat from './images/chat.png'
 import smalllogo from './images/smalllogo.png'
 
 export const assests = {
     logo,
-    chat,
     smalllogo,
 }
